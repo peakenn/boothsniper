16c0fb765b771148a132a2a5cf4bb71c26cd4a6c
peakenn <155244086+peakenn@users.noreply.github.com>
2024-01-03 00:23:04 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -151,11 +151,14 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
             ping = true
 	end	
         processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)		
-    elseif type.exclusiveLevel and price <= 70000 and item ~= "Banana" and item ~= "Coin" then
+    elseif type.exclusiveLevel and price <= 60000 and item ~= "Banana" and item ~= "Coin" then
         local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
 	processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
     elseif string.find(item, "Exclusive") and price <= 70000 then
         local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+	processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
+    elseif item == "Titanic Christmas Present" and price <= 200000 then
+        local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)	
 	processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping) 
     elseif type.titanic and price <= 10000000 then
         local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
